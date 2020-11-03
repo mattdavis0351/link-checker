1825b44c4db3cb3cab610a4eb4a8847b84686c2b
mattdavis0351 <mattdavis0351@github.com>
2020-11-03 19:46:11 +0000

    make code more modular

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
-	"net/http"
-	"regexp"
+
+	"github.com/mattdavis0351/link-checker/links"
 )
 
 // /([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#]?[\w-]+)*\/?/gm
@@ -15,44 +14,15 @@ import (
 // 	if there is a link make a GET request to the link
 // 		status 200 -> link is valid... count it as a link in the repo
 // 		status !200 > link is not valid, count it, report as broken
-// Write results to an issue on GitHub
+// Set the resulting output as environment variable using actions workflow commands
 var good, bad []string
-
-func parseFileForLinks(f string) [][]byte {
-	content, err := ioutil.ReadFile(f)
-	if err != nil {
-		fmt.Println("something went terribly wrong")
-	}
-	re := regexp.MustCompile(`([\w+]+\:\/\/)?([\w\d-]+\.)*[\w-]+[\.\:]\w+([\/\?\=\&\#]?[\w-]+)*\/?`)
-	u := re.FindAll(content, -1)
-	return u
-}
 
 func main() {
 
-	urls := parseFileForLinks("test-txt-file.txt")
+	urls := links.ParseFile("test-txt-file.txt")
 
-	for _, v := range urls {
-		sv := string(v)
-		resp, err := http.Get(sv)
+	allLinks := links.Links(urls)
 
-		// http status code do not count as err
-		if err != nil {
-			fmt.Println(err)
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != 200 {
-			// fmt.Printf("Not 200, got: %d", resp.StatusCode)
-			bad = append(bad, sv)
-		} else {
-			good = append(good, sv)
-		}
-		// fmt.Printf("%s\n",v)
-
-	}
-	// fmt.Println(s)
-	fmt.Println(good)
-	fmt.Println(bad)
+	fmt.Println(allLinks)
 
 }
