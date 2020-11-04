1e4dce3633c1ef35aea0638514e962fe02d9143c
mattdavis0351 <mattdavis0351@github.com>
2020-11-04 14:23:14 -0800

    ensure splitting of inputs is valid

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/mattdavis0351/link-checker/actions"
 	"github.com/mattdavis0351/link-checker/files"
@@ -19,11 +20,15 @@ import (
 // Set the resulting output as environment variable using actions workflow commands
 var ws string = os.Getenv("GITHUB_WORKSPACE")
 var ignoreExtenstions string = actions.GetInput("ignored_extensions")
+var includeExtenstions string = actions.GetInput("included_extensions")
 
 func main() {
-
+	ie := strings.Split(ignoreExtenstions, "")
+	iex := strings.Split(includeExtenstions, "")
 	fn := files.ReadWorkspaceDir()
 	fmt.Println(fn)
 	fmt.Printf("ignored extensions are:\n%s", ignoreExtenstions)
+	fmt.Printf("length of ignore extensions slice: %d\n", len(ie))
+	fmt.Printf("length of included extensions slice: %d\n", len(iex))
 
 }
