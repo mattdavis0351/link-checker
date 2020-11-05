7a931c5d9310cfce900fb8cd5e26b0e55266675a
mattdavis0351 <mattdavis0351@github.com>
2020-11-04 17:02:48 -0800

    filepath walk for file detection

--- files/workspace.go
+++ files/workspace.go
@@ -1,24 +1,91 @@
 package files
 
 import (
-	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/mattdavis0351/link-checker/actions"
+)
+
+var (
+	githubWorkspace    string = os.Getenv("GITHUB_WORKSPACE")
+	ignoreExtenstions  string = actions.GetInput("ignored_extensions")
+	includeExtenstions string = actions.GetInput("included_extensions")
 )
 
 func ReadWorkspaceDir() []string {
-	files, err := ioutil.ReadDir(os.Getenv("GITHUB_WORKSPACE"))
-	var fileNames []string
+	// files, err := ioutil.ReadDir(os.Getenv("GITHUB_WORKSPACE"))
+	// var fileNames []string
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// // TODO check if current item is file or dir
+	// // if dir then walk the tree getting out all the filenames to append
+	// // if file, then append the filename
+	// for _, file := range files {
+	// 	if file.IsDir() {
+	// 		f, err := ioutil.ReadDir(file.Name())
+	// 		if err != nil {
+	// 			log.Fatal(err)
+	// 		}
+	// 		for _, x := range f {
+	// 			if x.IsDir() {
+	// 				a, err := ioutil.ReadDir(x.Name())
+	// 				if err != nil {
+	// 					log.Fatal()
+	// 				}
+	// 				for _, y := range a {
+	// 					fmt.Println("use recursion")
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	fileNames = append(fileNames, file.Name())
+
+	// 	fmt.Println(file.Name())
+
+	// }
+	// return fileNames
+	var filesToParse []string
+	// ign := strings.Split(ignoreExtenstions, ",")
+	// inc := strings.Split(includeExtenstions, ",")
+	err := filepath.Walk(os.Getenv(githubWorkspace),
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				log.Println(err)
+			}
+			filesToParse = append(filesToParse, info.Name())
+			// for _, i := range inc {
+			// 	for _, e := range ign {
+			// 		if strings.Contains(info.Name(), i) || !strings.Contains(info.Name(), e) {
+			// 			filesToParse = append(filesToParse, info.Name())
+			// 		}
+			// 	}
+			// }
+
+			return nil
+		})
 	if err != nil {
-		log.Fatal(err)
+		log.Panicln(err)
 	}
-	for _, file := range files {
-		fileNames = append(fileNames, file.Name())
+	return filesToParse
+}
 
-		fmt.Println(file.Name())
+func PrepareToParse(p string, inc, exc []string) []string {
+	var filesToParse []string
+
+	for _, i := range inc {
+		for _, e := range exc {
+			if strings.Contains(p, i) || !strings.Contains(p, e) {
+				filesToParse = append(filesToParse, p)
+			}
+		}
 
 	}
-	return fileNames
+	// if the file.Name() includes extension or does not exclude extension
+
+	return filesToParse
 
 }
--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"os"
-	"strings"
 
-	"github.com/mattdavis0351/link-checker/actions"
 	"github.com/mattdavis0351/link-checker/files"
 )
 
@@ -18,17 +15,10 @@ import (
 // 		status 200 -> link is valid... count it as a link in the repo
 // 		status !200 > link is not valid, count it, report as broken
 // Set the resulting output as environment variable using actions workflow commands
-var ws string = os.Getenv("GITHUB_WORKSPACE")
-var ignoreExtenstions string = actions.GetInput("ignored_extensions")
-var includeExtenstions string = actions.GetInput("included_extensions")
 
 func main() {
-	ie := strings.Split(ignoreExtenstions, ",")
-	iex := strings.Split(includeExtenstions, ",")
+
 	fn := files.ReadWorkspaceDir()
 	fmt.Println(fn)
-	fmt.Printf("ignored extensions are:\n%s", ie)
-	fmt.Printf("length of ignore extensions slice: %d\n", len(ie))
-	fmt.Printf("length of included extensions slice: %d\n", len(iex))
 
 }
