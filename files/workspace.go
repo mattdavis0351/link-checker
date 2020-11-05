f02b8238993064cdc5f491f1880b8ae8b1f99f2f
mattdavis0351 <mattdavis0351@github.com>
2020-11-04 17:19:49 -0800

    omit .git and .github from filepath.walk

--- files/workspace.go
+++ files/workspace.go
@@ -56,7 +56,10 @@ func ReadWorkspaceDir() []string {
 			if err != nil {
 				log.Println(err)
 			}
-			filesToParse = append(filesToParse, info.Name())
+			if info.IsDir() && (info.Name() == ".git" || info.Name() == ".github") {
+				return filepath.SkipDir
+			}
+			filesToParse = append(filesToParse, path)
 			// for _, i := range inc {
 			// 	for _, e := range ign {
 			// 		if strings.Contains(info.Name(), i) || !strings.Contains(info.Name(), e) {
