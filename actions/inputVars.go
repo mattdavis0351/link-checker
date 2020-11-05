a7c55bf4258bbfa4e7b1e502b18de71cd2010e74
mattdavis0351 <mattdavis0351@github.com>
2020-11-05 08:18:11 -0800

    remove package declaration since vars are part of package now

--- actions/inputVars.go
+++ actions/inputVars.go
@@ -4,6 +4,6 @@ import "os"
 
 var (
 	GithubWorkspace    string = os.Getenv("GITHUB_WORKSPACE")
-	IgnoreExtenstions  string = actions.GetInput("ignored_extensions")
-	IncludeExtenstions string = actions.GetInput("included_extensions")
+	IgnoreExtenstions  string = GetInput("ignored_extensions")
+	IncludeExtenstions string = GetInput("included_extensions")
 )
