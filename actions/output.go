e7f02b9ca03d610bea1681fa1dd4bd0abecf359e
mattdavis0351 <mattdavis0351@github.com>
2020-11-06 14:08:11 -0800

    are spaces bad

--- actions/output.go
+++ actions/output.go
@@ -7,9 +7,10 @@ import (
 // echo "::workflow-command parameter1={data},parameter2={data}::{command value}"
 
 func SetOutput() error {
-	_, err := fmt.Println("::set-output name=link_objects::an array of objects")
+	_, err := fmt.Println("::set-output name=link_objects::objects")
 	if err != nil {
 		return err
 	}
+
 	return nil
 }
