af4c6799db957fa800a1b067c669d9f8c38999e3
mattdavis0351 <mattdavis0351@github.com>
2020-11-06 14:00:47 -0800

    try with println

--- actions/output.go
+++ actions/output.go
@@ -2,13 +2,12 @@ package actions
 
 import (
 	"fmt"
-	"os"
 )
 
 // echo "::workflow-command parameter1={data},parameter2={data}::{command value}"
 
 func SetOutput() error {
-	_, err := fmt.Fprintln(os.Stdout, "::set-output name=link_objects::an array of objects")
+	_, err := fmt.Println("::set-output name=link_objects::an array of objects")
 	if err != nil {
 		return err
 	}
